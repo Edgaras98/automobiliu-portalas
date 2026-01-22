c76ce04fba3f3eb4498b356128e34cf8818b2877
Edgaras98 <edgarasstepanovas14@gmail.com>
2026-01-22 20:45:37 +0200

    pridejau formos logika

---
+++ script.js
@@ -0,0 +1,33 @@
+const carForm = document.getElementById("carForm");
+const carsDiv = document.getElementById("cars");
+
+carForm.addEventListener("submit", function (event) {
+  // Sustabdys standartine formos elgsena, nepekraus puslapio
+  event.preventDefault();
+
+  // paimu info is formos kai ji paspaudziama
+  const name = document.getElementById("name").value;
+  const description = document.getElementById("description").value;
+  const price = document.getElementById("price").value;
+  const image = document.getElementById("image").value;
+  const fuel = document.getElementById("fuel").value;
+
+  // sukuriu automobilio kortele
+  const carCard = document.createElement("div");
+  carCard.className = "car-card";
+
+  // pripildau kortele informacija
+  carCard.innerHTML = `
+  <img src=${image} alt="car">
+  <h3>${name}</h3>
+  <p>${description}</p>
+  <p>Automobilio kaina: ${price}</p>
+  <p>Automobilio kuras: ${fuel}</p>
+  `;
+
+  // pridedu kortele i HTML'a
+  carsDiv.appendChild(carCard);
+
+  // Atsatyk forma i pradine padeti
+  carForm.reset();
+});
